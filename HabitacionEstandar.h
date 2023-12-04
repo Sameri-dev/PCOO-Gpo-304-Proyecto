96a45fe5c91d07b09077fb41942127b97f6de927
Sameri-dev <142357122+Sameri-dev@users.noreply.github.com>
2023-12-03 23:34:11 -0600

    Create HabitacionEstandar.h

---
+++ HabitacionEstandar.h
@@ -0,0 +1,8 @@
+//Abrimos una clase para la habitacion estandar (herencia)
+class HabitacionEstandar : public Habitacion {
+//datos publicos
+public:
+//contructor para la asiganacion de la habitacion estandar
+    HabitacionEstandar(int numero)
+        : Habitacion("A", (numero % 2 == 0) ? numero : numero + 1) {}
+};
