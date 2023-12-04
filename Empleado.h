2dc67c3a8651d95415d6b67ed118109501e3c562
Sameri-dev <142357122+Sameri-dev@users.noreply.github.com>
2023-12-03 23:24:01 -0600

    Create Empleado.h

---
+++ Empleado.h
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+//Creamos la clase del empleado/usuario (me confundia asi que lo cambie)
+class Empleado {
+//Seleccionamos los datos en privado
+private:
+//nombre
+    string nombre;
+//puesto
+    string puesto;
+//nomina
+    int nomina;
+//los datos publicos seran:
+public:
+//se contruyo el empleado usando el nombre, puesto y la nomina
+    Empleado(string nom, string pue, int non){ 
+    nombre = nom;
+    puesto = pue; 
+    nomina = non;
+    }
+//funcion para que se presentaen los datos del empleado ordenados y en 1 linea
+    void mostrarDatos() {
+        cout << "Empleado: " << nombre << " - Puesto: " << puesto << " - Nómina: " << nomina << endl;
+    }
+};
